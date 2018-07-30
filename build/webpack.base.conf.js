c46600ae494f2e89d984f712da181b87bbb66c9a
kenmick <louisluchen@gmail.com>
2018-07-30 23:12:41 +0800

    add navigation bar

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -36,6 +36,8 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      'components': resolve('src/components'), // set path alias for components
+      'common': resolve('src/common')
     }
   },
   module: {
--- src/main.js
+++ src/main.js
@@ -1,13 +1,30 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App'
+import goods from 'components/goods/goods'
+import ratings from 'components/ratings/ratings'
+import seller from 'components/seller/seller'
+
+import 'common/stylus/index.styl'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
+Vue.use(VueRouter)
+var router = new VueRouter({
+  routes: [
+    {path: '/goods', component: goods},
+    {path: '/ratings', component: ratings},
+    {path: '/seller', component: seller}
+  ],
+  linkActiveClass: 'active'
 })
+
+new Vue({
+  components: {App},
+  template: '<App/>',
+  router
+}).$mount('#app')
+
+router.push('/goods')
