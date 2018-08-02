791b1ca320b5e692ced14759a31065db60fd4fa9
kenmick <louisluchen@gmail.com>
2018-08-02 23:41:56 +0800

    add avatar and description part in header

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,11 +1,27 @@
 <template>
-  <div>I am goods</div>
+  <div class="goods">
+    <div class="menu-wrapper"></div>
+    <div class="foods-wrapper"></div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-export default {}
+export default {
+}
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  .goods
+    display: flex
+    position: absolute
+    width: 100%
+    top: 174px
+    bottom: 46px
+    overflow: hidden
+    .menu-wrapper
+      flex: 0 0 80px
+      width: 80px
+      background: #f3f5f7
+    .foods-wrapper
+      flex: 1
 </style>
