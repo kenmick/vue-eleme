9af4192855771468d0f1dd64cd971ef6a05e537f
kenmick <louisluchen@gmail.com>
2018-08-03 09:39:33 +0800

    add bulletin part and background in header

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -50,6 +50,8 @@ export default {
   @import "../../common/stylus/mixin"
 
   .header
+    position: relative
+    overflow: hidden
     color: #fff
     background: rgba(7, 17, 27, 0.5)
     .content-wrapper
@@ -133,7 +135,7 @@ export default {
       .bulletin-title
         display: inline-block
         vertical-align: top
-        margin-top: 7px
+        margin-top: 8px
         width: 22px
         height: 12px
         bg-image('bulletin')
